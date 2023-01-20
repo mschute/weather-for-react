b5fd05cfc78b2fdd8579098c0abc5ef7eda37266
mschute <46330682+mschute@users.noreply.github.com>
2023-01-20 08:54:28 +0000

    Migrated codesandbox react project to VS Code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./App.css";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function App(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+export default function App() {
+  const [city, setCity] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [description, setDescription] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [wind, setWind] = useState("");
+  const [icon, setIcon] = useState("");
+  const [loaded, setLoaded] = useState(false);
+
+  let form = (
+    <form onSubmit={handleQuery}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        autoFocus={true}
+        onChange={updateCity}
+      ></input>
+      <input type="submit" value="Search"></input>
+    </form>
+  );
+
+  function handleQuery(event) {
+    event.preventDefault();
+    let unit = "metric";
+    let apiKey = "27cc38c68db54a86823a86663308e49a";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(url).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function showWeather(response) {
+    setLoaded(true);
+    setTemperature(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
   }
 
-  let apiKey = "27cc38c68db54a86823a86663308e49a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return <h1>Weather App</h1>;
+  if (loaded) {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        {form}
+        <ul>
+          <li>City: {city}</li>
+          <li>Temperature: {Math.round(temperature)}°C</li>
+          <li>Description: {description}</li>
+          <li>Humidity: {humidity}%</li>
+          <li>Wind: {wind} km/h</li>
+          <li>
+            <img src={icon} alt={description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        {form}
+      </div>
+    );
+  }
 }
