64f60a9b24740ee4d87718eb5279da69e3f7b66f
mschute <46330682+mschute@users.noreply.github.com>
2023-01-20 11:18:45 +0000

    Imported bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App";
 
