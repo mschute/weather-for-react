cf8b896ecce813804c5fecf3cae44a74a215cc73
mschute <46330682+mschute@users.noreply.github.com>
2023-01-26 11:35:35 +0000

    Create new Weather Response component

--- src/Weather.js
+++ src/Weather.js
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.css";
 import axios from "axios";
-import CurrentDate from "./CurrentDate";
+import WeatherResults from "./WeatherResults";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     setWeatherData({
@@ -14,79 +15,65 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       wind: response.data.wind.speed,
       date: new Date(response.data.time * 1000),
+      city: response.data.city,
       description: response.data.condition.description,
       iconUrl: response.data.condition.icon_url,
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCity(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "f4ff5751e00t63c15a8eb8eo1612abfe";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (weatherData.ready) {
     const apiKey = "f4ff5751e00t63c15a8eb8eo1612abfe";
-    let city = props.city;
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    console.log({ apiUrl });
     axios.get(apiUrl).then(handleResponse);
 
     return (
       <div className="Weather">
         <h1>Weather Forecast</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="search"
             placeholder="Enter a city..."
             className="form-control"
             autoComplete="off"
+            onChange={handleCity}
           />
           <input type="submit" value="Search" className="search-button" />
         </form>
-        <div class="row current-stats">
-          <div class="col-1 icon">
-            <img
-              src={weatherData.iconUrl}
-              alt="weather icon"
-              className="current-icon"
-            />
-          </div>
-          <ul className="col-6 current-weather">
-            <li className="current-temp">
-              {Math.round(weatherData.temperature)}
-              <sup className="temperature-scales">
-                <div className="celsius">
-                  <a href className="celsius-link active">
-                    {" "}
-                    °C{" "}
-                  </a>
-                </div>
-                |
-                <div className="fahrenheit">
-                  <a href className="fahrenheit-link">
-                    {" "}
-                    °F
-                  </a>
-                </div>
-              </sup>
-            </li>
-            <li className="hum-wind">
-              Humidity <span className="humidity">{weatherData.humidity}</span>%
-              <br />
-              Wind <span className="wind">{weatherData.wind}</span>{" "}
-              <span className="speed">kph</span>
-            </li>
-          </ul>
-          <ul className="col-5 location">
-            <li className="current-city">{props.city}</li>
-            <li className="day-time">
-              <CurrentDate date={weatherData.date} />
-            </li>
-            <li className="current-weather-desc">{weatherData.description}</li>
-          </ul>
-        </div>
+        <WeatherResults data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "f4ff5751e00t63c15a8eb8eo1612abfe";
-    let city = props.city;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
-    return "Loading...";
+    search();
+    return (
+      <div className="Weather">
+        <h1>Weather Forecast</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            className="form-control"
+            autoComplete="off"
+            onChange={handleCity}
+          />
+          <input type="submit" value="Search" className="search-button" />
+        </form>
+      </div>
+    );
   }
 }
---
+++ src/WeatherResults.js
@@ -0,0 +1,51 @@
+import React from "react";
+import CurrentDate from "./CurrentDate";
+
+export default function WeatherResults(props) {
+  return (
+    <div className="WeatherResults">
+      <div class="row current-stats">
+        <div class="col-1 icon">
+          <img
+            src={props.data.iconUrl}
+            alt="weather icon"
+            className="current-icon"
+          />
+        </div>
+        <ul className="col-6 current-weather">
+          <li className="current-temp">
+            {Math.round(props.data.temperature)}
+            <sup className="temperature-scales">
+              <div className="celsius">
+                <a href className="celsius-link active">
+                  {" "}
+                  °C{" "}
+                </a>
+              </div>
+              |
+              <div className="fahrenheit">
+                <a href className="fahrenheit-link">
+                  {" "}
+                  °F
+                </a>
+              </div>
+            </sup>
+          </li>
+          <li className="hum-wind">
+            Humidity <span className="humidity">{props.data.humidity}</span>%
+            <br />
+            Wind <span className="wind">{props.data.wind}</span>{" "}
+            <span className="speed">kph</span>
+          </li>
+        </ul>
+        <ul className="col-5 location">
+          <li className="current-city">{props.data.city}</li>
+          <li className="day-time">
+            <CurrentDate date={props.data.date} />
+          </li>
+          <li className="current-weather-desc">{props.data.description}</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
