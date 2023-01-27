caef102cd416770bba681f8f6fab270788e8e449
mschute <46330682+mschute@users.noreply.github.com>
2023-01-27 12:52:07 +0000

    Added unit conversion component
    Added the ability to switch between celsius and Fahrenheit while also being able to switch between kilometers per hour and miles per hour

--- src/WeatherResults.js
+++ src/WeatherResults.js
@@ -1,5 +1,7 @@
 import React from "react";
 import CurrentDate from "./CurrentDate";
+import UnitConversion from "./UnitConversion";
+import "./WeatherResults.css";
 
 export default function WeatherResults(props) {
   return (
@@ -13,37 +15,20 @@ export default function WeatherResults(props) {
           />
         </div>
         <ul className="col-6 current-weather">
-          <li className="current-temp">
-            {Math.round(props.data.temperature)}
-            <sup className="temperature-scales">
-              <div className="celsius">
-                <a href className="celsius-link active">
-                  {" "}
-                  °C{" "}
-                </a>
-              </div>
-              |
-              <div className="fahrenheit">
-                <a href className="fahrenheit-link">
-                  {" "}
-                  °F
-                </a>
-              </div>
-            </sup>
-          </li>
-          <li className="hum-wind">
-            Humidity <span className="humidity">{props.data.humidity}</span>%
-            <br />
-            Wind <span className="wind">{props.data.wind}</span>{" "}
-            <span className="speed">kph</span>
-          </li>
+          <UnitConversion
+            celsius={props.data.temperature}
+            kilometers={props.data.wind}
+            humidity={props.data.humidity}
+          />
         </ul>
         <ul className="col-5 location">
           <li className="current-city">{props.data.city}</li>
           <li className="day-time">
             <CurrentDate date={props.data.date} />
           </li>
-          <li className="current-weather-desc">{props.data.description}</li>
+          <li className="current-weather-desc text-capitalize">
+            {props.data.description}
+          </li>
         </ul>
       </div>
     </div>
