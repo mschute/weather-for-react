1d4bab13cc63e0bc0a28c3521b46ed14b4b06fbe
mschute <46330682+mschute@users.noreply.github.com>
2023-02-10 14:55:13 +0000

    Removed city as extra variable

--- src/Weather.js
+++ src/Weather.js
@@ -7,7 +7,6 @@ import WeatherForecast from "./WeatherForecast";
 
 export default function Weather(props) {
   const [unit, setUnit] = useState("celsius");
-  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleUnit(unit) {
@@ -26,6 +25,7 @@ export default function Weather(props) {
       description: response.data.condition.description,
       iconUrl: response.data.condition.icon_url,
     });
+    console.log(weatherData);
   }
 
   function handleSubmit(event) {
@@ -34,12 +34,12 @@ export default function Weather(props) {
   }
 
   function handleCity(event) {
-    setCity(event.target.value);
+    setWeatherData({ city: event.target.value, ready: false });
   }
 
   function searchWeather() {
     const apiKey = "f4ff5751e00t63c15a8eb8eo1612abfe";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${weatherData.city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -67,7 +67,21 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    searchWeather();
-    return "Searching...";
+    return (
+      <div className="Weather">
+        <h1>Weather Forecast</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            className="form-control"
+            autoComplete="off"
+            autoFocus="on"
+            onChange={handleCity}
+          />
+          <input type="submit" value="Search" className="search-button" />
+        </form>
+      </div>
+    );
   }
 }
